2e26bc7f20926a41d566726b7a593b4f920eb0c6
varshavigasini080 <varshavigasini080@gmail.com>
2021-08-21 23:44:23 +0530

    first commit

--- JobRegistration/src/app/app.component.ts
+++ JobRegistration/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'JobRegistration';
+  isChecked = true;
+
+  onChange($event:any){
+    console.log($event);
+  }
 }
